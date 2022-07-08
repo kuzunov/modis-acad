71341a42d6a7c3799c6aaa383eb35949f72c72b0
Konstantin Uzunov <60385895+kuzunov@users.noreply.github.com>
2022-07-08 18:24:25 +0300

    Breaking stuff

--- HW/books-search-plain-js/search.js
+++ HW/books-search-plain-js/search.js
@@ -1,11 +1,46 @@
-const searchBox = document.getElementById("search-box");
+const searchBoxes = document.getElementsByClassName("search-box");
 const errorBox = document.getElementById("error");
 const searchBy = document.getElementById("search-by-input");
+const checks = document.getElementsByClassName("checks");
+const searchWrapper = document.getElementById("search-wrapper")
 
-searchBox.addEventListener("input", (e) => inputChange(e.target.value,searchBy.value));
-searchBy.addEventListener("input", (e) => inputChange(searchBox.value,e.target.value));
+// searchBox.forEa.addEventListener("input", (e) => inputChange(e.target.value,searchBy.value));
+// searchBy.addEventListener("input", (e) => inputChange(searchBox.value,e.target.value));
 
+class SearchField {
+    constructor(type){
+        const submit = document.getElementById("submit-btn");
+        submit.addEventListener("click", this.handleSubmit())
+        const newEl = document.createElement('div');
+        newEl.className = type;
+        let checkBox = document.createElement("input");
+        const cbProps = {type: "checkBox", className:"checks",id:`search-${type.toLowerCase()}-check`,innerHTML:type}
+        // checkBox.type = "checkbox"
+        // checkBox.className = "checks";
+        // checkBox.id = `search-${type.toLowerCase()}-check`
+        // checkBox.innerHTML = type;
+        checkBox = {...checkBox,...cbProps};
+        console.log(checkBox)
+        const textField = document.createElement("input");
+        textField.type = "text";
+        textField.className = "search-box";
+        textField.id = `search-${type.toLowerCase()}-field`
+        // newEl.innerHTML = `<input type="checkbox" class = "checks" id=search-${type.toLowerCase()}-check>${type}</input>
+        //                     <input type="text" class="search-box" id=search-${type.toLowerCase()}-field></input>`
+        checkBox.addEventListener();
+        // textField.addEventListener("input", (e) => {this.inputChange(e.target.value)})
+        newEl.appendChild(checkBox);
+        newEl.appendChild(textField);
+        searchWrapper.appendChild(newEl);
+    }
+    inputChange() {
 
+    }
+    handleSubmit = () => {
+
+    }
+}
+let test = new SearchField("Author");
 
 
 const inputChange = () => {
@@ -23,3 +58,6 @@ const handleSearch = (searchStr,searchBy)=>{
         searchIn=searchIn+searchBy.toLowerCase()+":"+searchStr.toLowerCase();
     console.log(API_URL+searchIn+"&key="+GAPI_KEY);
 };
+const handleCheck = () => {
+
+}
