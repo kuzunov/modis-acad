9b21a2ed8acd2fb2f53f24d1e205d6c4b24f0095
Konstantin Uzunov <60385895+kuzunov@users.noreply.github.com>
2022-07-09 17:27:01 +0300

    Doing lots of stuff (design,ContentManager,search works, API works, refactoring, preparing for broadSearch) + adding comments

--- HW/books-search-plain-js/SearchBox.js
+++ HW/books-search-plain-js/SearchBox.js
@@ -1,26 +1,51 @@
 import { API_URL,GAPI_KEY } from "./constants.js";
 export class SearchBox {
-    constructor(fields,wrapper) {
+    //fields for specific search, wrapper and manager Fn from ContentManager
+    constructor(fields,wrapper,managerFn) {
         fields.map(f => {
             wrapper.appendChild(f.html);
         })
         let submit = document.createElement("button")
         submit.id="submit-btn";
         submit.innerHTML= "Submit";
-        submit.addEventListener("click", this.handleSubmit.bind(this))
+        submit.addEventListener("click",() => this.handleSubmit())
         wrapper.appendChild(submit);
         this.fields = fields;
+        this.manager = managerFn;
     }
-    handleSubmit(){
+    //search in all categories
+    broadSearch(){
         let searchURL=API_URL;
-        this.fields.map(f=> {
-            (!f.textField.hasAttribute("disabled")||!f.textField.value!=="") ? 
-                searchURL+=`in${f.type.toLowerCase()}:${f.textField.value}+`:
-                searchURL+=""
-        })
-        // searchIn=searchIn+searchBy.toLowerCase()+":"+searchStr.toLowerCase();
-        searchURL+=`&key=${GAPI_KEY}`;
-        console.log(searchURL);
-        //async//await//split string for multiple keywords
     }
+    //specific search
+    handleSubmit(){
+        //build url
+        let searchURL=API_URL;
+        this.fields.forEach((f)=> {
+            if (!f.textField.hasAttribute("disabled")&&f.textField.value!=="") { 
+                //category:split+keywords
+                searchURL+=`in${f.type.toLowerCase()}:${f.textField.value.replace(" ","+")}+`
+            }
+        })
+        //remove last + and append API_KEY
+        searchURL=`${searchURL.slice(0,-1)}&key=${GAPI_KEY}`;
+        //call manager Fn with URL
+        this.manager(searchURL);
+        
+    }
+    // const inputChange = () => {
+//    if (searchBox.value.length>2&&searchBy.value) { 
+//          errorBox.style.display= "none"; 
+//         handleSearch(searchBox.value,searchBy.value); 
+//     } 
+//     else {
+//         errorBox.style.display= "flex";
+//         errorBox.innerHTML="Type 3 or more characters and select a category to search!";
+//     }
+// }
+// const handleSearch = (searchStr)=>{
+//     let searchIn = "in";
+//         searchIn=searchIn+searchBy.toLowerCase()+":"+searchStr.toLowerCase();
+//     console.log(API_URL+searchIn+"&key="+GAPI_KEY);
+// };
 }
--- HW/books-search-plain-js/search.js
+++ HW/books-search-plain-js/search.js
@@ -1,10 +1,10 @@
 const errorBox = document.getElementById("error");
-const searchBy = document.getElementById("search-by-input");
 const searchWrapper = document.getElementById("search-wrapper")
-const submit = document.getElementById("submit-btn");
+const contentWrapper = document.getElementById("content-wrapper")
 
 import { SearchField } from './SearchField.js';
 import { SearchBox } from './SearchBox.js';
+import { ContentManager } from './ContentManager.js';
 // searchBox.forEa.addEventListener("input", (e) => inputChange(e.target.value,searchBy.value));
 // searchBy.addEventListener("input", (e) => inputChange(searchBox.value,e.target.value));
 
@@ -13,22 +13,7 @@ let fields = [new SearchField("Author"),
             new SearchField("Title"),
             new SearchField("Publisher"),
             new SearchField("Subject")];
-let searchBox = new SearchBox(fields,searchWrapper);
-const inputChange = () => {
-   if (searchBox.value.length>2&&searchBy.value) { 
-         errorBox.style.display= "none"; 
-        handleSearch(searchBox.value,searchBy.value); 
-    } 
-    else {
-        errorBox.style.display= "flex";
-        errorBox.innerHTML="Type 3 or more characters and select a category to search!";
-    }
-}
-const handleSearch = (searchStr,searchBy)=>{
-    let searchIn = "in";
-        searchIn=searchIn+searchBy.toLowerCase()+":"+searchStr.toLowerCase();
-    console.log(API_URL+searchIn+"&key="+GAPI_KEY);
-};
-const handleCheck = () => {
+let cM = new ContentManager([],contentWrapper);
+let searchBox = new SearchBox(fields,searchWrapper,cM.getBooks);
 
-}
+
