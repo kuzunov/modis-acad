43addda090e653262378f4883b5a29f8ba7ebc78
Konstantin Uzunov <60385895+kuzunov@users.noreply.github.com>
2022-07-10 11:43:14 +0300

    removing unused stuff

--- HW/books-search-plain-js/search.js
+++ HW/books-search-plain-js/search.js
@@ -4,8 +4,6 @@ const contentWrapper = document.getElementById("content-wrapper");
 import { SearchField } from "./SearchField.js";
 import { SearchBox } from "./SearchBox.js";
 import { ContentManager } from "./ContentManager.js";
-// searchBox.forEa.addEventListener("input", (e) => inputChange(e.target.value,searchBy.value));
-// searchBy.addEventListener("input", (e) => inputChange(searchBox.value,e.target.value));
 
 let fields = [
 	new SearchField("Author"),
